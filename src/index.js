7744f62630d62615aaf46eb70716b82358e1f813
athul <athuljoshi7@gmail.com>
2021-05-21 14:26:25 +0530

    resolved conflicts

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { createStore } from "redux";
 import { Provider } from "react-redux";
 import App from "./App";
 import rootReducer from "./store/rootReducer";
-<<<<<<< HEAD
 import { createFirestoreInstance } from "redux-firestore";
 import { ReactReduxFirebaseProvider } from "react-redux-firebase";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -13,14 +12,6 @@ import {
   setToLocalStorage,
   getFromLocalStorage,
 } from "./store/persistenceFuncs";
-=======
-import {createFirestoreInstance} from "redux-firestore";
-import {ReactReduxFirebaseProvider} from "react-redux-firebase";
-import firebase  from "./firebase";
-import {setToLocalStorage, getFromLocalStorage} from "./store/persistenceFuncs";
-import 'bootstrap/dist/css/bootstrap.min.css';
-
->>>>>>> styled the dashboard
 
 const rrfConfig = {
   userProfile: "users",
