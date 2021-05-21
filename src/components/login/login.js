40d12d28322a93909d3ad60fcf18712947f0ebea
iamAravindks <aravindkadaikattu@gmail.com>
2021-05-21 12:01:24 +0530

    Login page styled

--- src/components/login/login.js
+++ src/components/login/login.js
@@ -1,64 +1,107 @@
 import loginStyles from "./styles/loginStyles.module.css";
-import {useRef, useState} from "react";
-import {useSelector} from "react-redux";
-import {useHistory} from 'react-router-dom';
-import {useFirebase} from "react-redux-firebase";
-import {isLoaded, isEmpty} from "react-redux-firebase";
-
+import { useRef, useState } from "react";
+import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
+import { useFirebase } from "react-redux-firebase";
+import { isLoaded, isEmpty } from "react-redux-firebase";
+import { Button, Container, Form, Row } from "react-bootstrap";
+import Img from "../../assets/images/logo.png";
 
 const Login = () => {
-    const [buttonDisabled, setButtonDisabled] = useState(false);
-    const [credentialError, setCredentialError] = useState(null);
-    const emailVal = useRef(null);
-    const passwordVal = useRef(null);
-    const history = useHistory();
-    const firebase = useFirebase();
+  const [buttonDisabled, setButtonDisabled] = useState(false);
+  const [credentialError, setCredentialError] = useState(null);
+  const emailVal = useRef(null);
+  const passwordVal = useRef(null);
+  const history = useHistory();
+  const firebase = useFirebase();
 
-    // TEST: delete it after use
-    const auth = useSelector(state => state.firebase.auth);
+  // TEST: delete it after use
+  const auth = useSelector((state) => state.firebase.auth);
 
+  const verifyEmailAndPassword = () => {
+    if (emailVal.current.value === "" || passwordVal.current.value === "") {
+      setCredentialError("Please Enter Email and password");
+      return 0;
+    }
+    return 1;
+  };
 
-    const verifyEmailAndPassword = () => {
-        if (emailVal.current.value === "" || passwordVal.current.value === "") {
-            setCredentialError("Please Enter Email and password")
-            return 0;
-        }
-        return 1;
-    };
+  const handleLoginButtonClick = (e) => {
+    e.preventDefault();
+    setButtonDisabled(true);
 
+    // TEST
+    if (true || verifyEmailAndPassword()) {
+      firebase
+        .login({
+          email: process.env.REACT_APP_TEST_EMAIL,
+          password: process.env.REACT_APP_TEST_PWORD,
+        })
+        .then((user) => {
+          history.push("/");
+        })
+        .catch((err) => {
+          setCredentialError(err);
+          emailVal.current.value = "";
+          passwordVal.current.value = "";
+          setButtonDisabled(false);
+        });
+    }
+  };
 
-    const handleLoginButtonClick = (e) => {
-        e.preventDefault();
-        setButtonDisabled(true);
-
-        // TEST
-        if (true || verifyEmailAndPassword()) {
-            firebase.login({email: process.env.REACT_APP_TEST_EMAIL, password: process.env.REACT_APP_TEST_PWORD})
-                .then((user) => {
-                    history.push('/');
-                })
-                .catch((err) => {
-                    setCredentialError(err);
-                    emailVal.current.value = "";
-                    passwordVal.current.value = "";
-                    setButtonDisabled(false);
-                });
-        }
-    };
-
-
-    return (
-        <div className={loginStyles.container}>
-            <div className={loginStyles.loginTextHeader}>Login</div>
-            <input type="text" ref={emailVal} placeholder={'email'} required />
-            <input type="password" ref={passwordVal} placeholder={'password'} required />
-            <div>{credentialError}</div>
-            <button disabled={buttonDisabled} onClick={(e) => handleLoginButtonClick(e)}>Log In</button>
-            {/*{isLoaded(auth)?<p>Auth is loaded</p>:<p>auth is not loaded</p>}*/}
-            {/*{!isEmpty(auth)?<p>User is authenticated</p>:<p>user is not authenticated</p>}*/}
+  return (
+    <Container
+      fluid
+      className={`${loginStyles.loginContainer} ${loginStyles.boxCenter}`}
+    >
+      <Row className={`${loginStyles.loginRow} ${loginStyles.boxCenter}`}>
+        <div
+          className={`col-12 ${loginStyles.sectionTop} ${loginStyles.boxCenter}`}
+        >
+          <img src={Img} alt='img here ' className={loginStyles.heroImg} />
+          <h1>ACGAM</h1>
+          <h3>Welcome Back :)</h3>
         </div>
-    );
-};
+        <div
+          className={`col-12 ${loginStyles.sectionBottom} ${loginStyles.boxCenter}`}
+        >
+          <Form className={`col-md-6 m-5 ${loginStyles.loginForm}`}>
+            <Form.Group controlId='formBasicEmail'>
+              <Form.Control
+                type='email'
+                placeholder='Enter email'
+                ref={emailVal}
+                required
+              />
+              <Form.Text className='text-muted'>
+                We'll never share your email with anyone else.
+              </Form.Text>
+            </Form.Group>
 
+            <Form.Group controlId='formBasicPassword'>
+              <Form.Control
+                type='password'
+                placeholder='Password'
+                ref={passwordVal}
+                required
+              />
+            </Form.Group>
+
+            <Button
+              size='sm'
+              variant='primary'
+              type='submit'
+              disabled={buttonDisabled}
+              onClick={(e) => handleLoginButtonClick(e)}
+              className={loginStyles.submitBtn}
+            >
+              Submit
+            </Button>
+          </Form>
+        </div>
+      </Row>
+    </Container>
+  );
+};
 
 export default Login;
--- src/index.js
+++ src/index.js
@@ -1,44 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {createStore} from 'redux';
-import {Provider} from "react-redux";
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import App from "./App";
 import rootReducer from "./store/rootReducer";
-import {createFirestoreInstance} from "redux-firestore";
-import {ReactReduxFirebaseProvider} from "react-redux-firebase";
-import firebase  from "./firebase";
-import {setToLocalStorage, getFromLocalStorage} from "./store/persistenceFuncs";
-
+import { createFirestoreInstance } from "redux-firestore";
+import { ReactReduxFirebaseProvider } from "react-redux-firebase";
+import "bootstrap/dist/css/bootstrap.min.css";
+import firebase from "./firebase";
+import {
+  setToLocalStorage,
+  getFromLocalStorage,
+} from "./store/persistenceFuncs";
 
 const rrfConfig = {
-    userProfile: "users",
-    useFirestoreForProfile: true,
+  userProfile: "users",
+  useFirestoreForProfile: true,
 };
-
 
 const persistedState = getFromLocalStorage();
 const store = createStore(rootReducer, persistedState);
 
 store.subscribe(() => setToLocalStorage(store.getState()));
 
-
 const rrfProps = {
-    firebase,
-    config: rrfConfig,
-    dispatch: store.dispatch,
-    createFirestoreInstance,
+  firebase,
+  config: rrfConfig,
+  dispatch: store.dispatch,
+  createFirestoreInstance,
 };
 
-
-
-
 ReactDOM.render(
-    <Provider store={store}>
-            <ReactReduxFirebaseProvider {...rrfProps}>
-                <React.StrictMode>
-                    <App />
-                </React.StrictMode>
-            </ReactReduxFirebaseProvider>
-    </Provider>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <ReactReduxFirebaseProvider {...rrfProps}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </ReactReduxFirebaseProvider>
+  </Provider>,
+  document.getElementById("root")
 );
